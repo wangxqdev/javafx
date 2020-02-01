332e5f2a5220bfcae875f5b121a3b5b5a2f751b0
wangxinquan <wangxinquan@anji-tec.com>
2020-02-01 22:56:42 +0800

    新增ComboBox

--- src/main/java/com/tec/anji/widget/combobox/Main.java
+++ src/main/java/com/tec/anji/widget/combobox/Main.java
@@ -1,63 +1,72 @@
-package com.tec.anji.widget.choicebox;
+package com.tec.anji.widget.combobox;
 
 import javafx.application.Application;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.SingleSelectionModel;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.StringConverter;
 
 /**
- * ChoiceBox
+ * ComboBox
  */
 public class Main extends Application
 {
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        TextField textField = new TextField();
-        textField.setStyle("-fx-font-size: 14px");
-        Button button = new Button("修改");
-        HBox hBox = new HBox(10, textField, button);
-        hBox.setStyle("-fx-alignment: center_left");
-
-        ChoiceBox<Student> choiceBox = new ChoiceBox<>();
-        choiceBox.setStyle("-fx-pref-width: 100px");
-        ObservableList<Student> students = choiceBox.getItems();
+        ComboBox<Student> comboBox = new ComboBox<>();
+        ObservableList<Student> students = comboBox.getItems();
         for (int i = 1; i <= 5; i++)
         {
             int value = (int) Math.round(Math.random() * 100);
             Student student = new Student(String.format("小%s", i), value > 50 ? value - 50 : value, value);
-//            属性值监听
-            student.nameProperty().addListener((observable, oldValue, newValue) ->
-            {
-                int index = students.indexOf(student);
-                students.remove(student);
-                students.add(index, student);
-            });
             students.add(student);
         }
+        comboBox.setEditable(true);
+        comboBox.setVisibleRowCount(5);
+        comboBox.setConverter(new StudentStringConverter());
 
-//        对象数据转换
-        choiceBox.setConverter(new StudentStringConverter());
-        SingleSelectionModel<Student> selectionModel = choiceBox.getSelectionModel();
-        selectionModel.selectedItemProperty().addListener((observable, oldValue, newValue) -> textField.setText(newValue.getName()));
-        selectionModel.select(0);
+        TextField textField = comboBox.getEditor();
+        textField.setOnMouseClicked(e -> {
+            textField.clear();
+            comboBox.setItems(students);
+            comboBox.show();
+        });
 
-        button.setOnAction(e -> selectionModel.getSelectedItem().setName(textField.getText()));
+        textField.setOnKeyReleased(e ->
+        {
+            String input = comboBox.getEditor().getText();
+            if (input.isEmpty())
+            {
+                comboBox.setItems(students);
+            }
+            else
+            {
+                ObservableList<Student> filteredStudents = students.filtered(stu -> stu.getName().contains(input));
+                if (!filteredStudents.isEmpty())
+                {
+                    comboBox.setItems(filteredStudents);
+                }
+                else
+                {
+                    comboBox.setItems(null);
+                }
+            }
+            comboBox.hide();
+            comboBox.show();
+        });
 
-        VBox root = new VBox(10, hBox, choiceBox);
-        root.setStyle("-fx-padding: 10px");
+        HBox root = new HBox(comboBox);
+        root.setStyle("-fx-padding: 20px; -fx-alignment: top_center");
 
-        primaryStage.setScene(new Scene(root, 500, 400));
-        primaryStage.setTitle("ChoiceBox");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("ComboBox");
         primaryStage.show();
     }
 
@@ -127,7 +136,7 @@ public class Main extends Application
         @Override
         public String toString(Student student)
         {
-            if (null == student)
+            if (student == null)
             {
                 return "";
             }
